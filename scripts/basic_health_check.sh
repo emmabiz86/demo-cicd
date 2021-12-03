f64fcf13d481a8ddb3abe519e0ccf32dc76e5f5f
emmabiz86 <68460077+emmabiz86@users.noreply.github.com>
2021-12-03 13:39:34 -0600

    Update basic_health_check.sh

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
+keyword="Bespoke"
 
 for i in `seq 1 10`;
 do
   HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
   if [ "$HTTP_CODE" == "200" ]; then
-    echo "Successfully pulled root page."
-    exit 0;
+    if (curl -s "http://localhost:80" -m 10 | grep "$keyword"); then
+            # if the keyword is in the conent
+        echo "Successfully pulled root page."
+        exit 0;
+    fi
   fi
   echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
   sleep 10
